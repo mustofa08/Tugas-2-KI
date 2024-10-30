75d620afb3443e930dda6c46b4279765e28da9b2
Akhmad Mustofa Solikin <91455464+mustofa08@users.noreply.github.com>
2024-10-31 00:16:49 +0700

    Update socket_client.py

--- socket_client.py
+++ socket_client.py
@@ -1,11 +1,11 @@
 import socket
-from des import encrypt_message
+from des import encrypt_message, decrypt_message
 
 KEY = "AABBCCDDEEFF1122"
 
 def client_program():
-    host = socket.gethostname()  # or use '127.0.0.1' if you run server on the same machine
-    port = 5000  # the port on which the server is listening
+    host = socket.gethostname()  # as both code is running on same pc
+    port = 5000  # socket server port number
 
     client_socket = socket.socket()  # instantiate
     client_socket.connect((host, port))  # connect to the server
@@ -13,13 +13,13 @@ def client_program():
     message = input(" -> ")  # take input
 
     while message.lower().strip() != 'bye':
-        encrypted_message = encrypt_message(message, KEY)  # encrypt the message before sending
-        print("Pesan terenkripsi yang dikirim: " + encrypted_message)  # print the encrypted message
-        client_socket.send(encrypted_message.encode())  # send the encrypted message
+        encrypted_message = encrypt_message(message, KEY)  # encrypt the message
+        client_socket.send(encrypted_message.encode())  # send message
+        print('Pesan terenkripsi yang dikirim: ' + encrypted_message)  # show sent message
 
-        # receive response from the server
-        data = client_socket.recv(1024).decode()  
-        print('Pesan dari server (terenkripsi): ' + data)  # show encrypted response from server
+        data = client_socket.recv(1024).decode()  # receive response
+        decrypted_data = decrypt_message(data, KEY)  # decrypt the response
+        print('Received from server: ' + decrypted_data)  # show in terminal
 
         message = input(" -> ")  # again take input
 
